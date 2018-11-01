23d20992539267b610ff54f4c7bfe5e80804a4bf
Andre-Tobois <tobois.andre@gmail.com>
2018-11-01 11:04:23 -0400

    Cards are now shuffled at start of game.

--- js/app.js
+++ js/app.js
@@ -17,16 +17,17 @@ const icons = [
   "fa fa-bomb",
   "fa fa-bomb"
 ];
-
+///global varaiables
 const cardsHolder = document.querySelector(".deck");
 const starIcon = `<li><i class="fa fa-star"></i></li>`
 let openCards = [];
 let pairCards = [];
-let intialClick = false;
+let firstClick = false;
 let clockOff = true;
 let time = 0;
 let clockId;
 let moves = 0;
+
 /* Display the cards on the page   - shuffle the list of cards using the provided "shuffle" method below   - loop through each card and create its HTML   - add each card's HTML to the page */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -48,10 +49,10 @@ function shuffle(array) {
 
 /* set up the event listener for a card. If a card is clicked:  - display the card's symbol (put this functionality in another function that you call from this one)  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)  - if the list already has another card, check to see if the two cards match    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one) */
 
-///Create the cards for the game
+///Creates the memory game
 
 function init() {
-  // const shuffleIcons = shuffle(icons)
+  const shuffleIcons = shuffle(icons)
   for (let i = 0; i < icons.length; i++) {
     const card = document.createElement("li");
     card.classList.add("card");
@@ -68,8 +69,8 @@ function click(card) {
   card.addEventListener("click", function() {
     const currCard = this;
     const prevCard = openCards[0];
-    if (intialClick === false) {
-      intialClick = true;
+    if (firstClick === false) {
+      firstClick = true;
       if (clockOff) {
         startTimer();
         addMove();
@@ -128,7 +129,7 @@ function toggleModal() {
   modal.classList.toggle("hide");
 }
 
-///Grabs the stars from the game and displays them for winner///
+///Grabs the stats from the game and displays them for winner///
 function fillInStats() {
   const timeStat = document.querySelector('.modal-time');
   const movesStat = document.querySelector('.modal-moves');
@@ -141,7 +142,7 @@ function fillInStats() {
 
 }
 
-///what must happen when game is over
+///When game is over clock with stop, modal appears with stats//
 function gameOver() {
   if (pairCards.length === icons.length) {
     stopClock();
@@ -152,7 +153,6 @@ function gameOver() {
 
 ///Ratings for game, stars will be removed based on how many clicks on deck///
 const starsHolder = document.querySelector(".stars");
-
 function rating() {
   switch (moves) {
     case 15:
@@ -180,7 +180,6 @@ function getStars() {
 function startTimer() {
   clockId = setInterval(() => {
     time++;
-    console.log(time);
     displayTime();
   }, 1000);
 }
@@ -188,7 +187,6 @@ function startTimer() {
 ///Shows the time after a card has been clicked///
 function displayTime() {
   const clock = document.querySelector('.clock');
-  console.log(clock);
   clock.innerHTML = time;
 
   const minutes = Math.floor(time / 60);
@@ -257,7 +255,7 @@ function reset() {
   movesHolder.innerHTML = moves;
   stopClock();
   displayTime(0);
-  intialClick = false;
+  firstClick = false;
   starsHolder.innerHTML = starIcon + starIcon + starIcon;
 }
 
